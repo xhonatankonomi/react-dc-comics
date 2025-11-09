ec6eb3d31e43502ceea7148c306a2aa8615af77a
xhonatankonomi <xhoni8962@gmail.com>
2025-11-09 21:51:56 +0100

    Add Logo component to display DC logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/dc-logo.png'
+
+const Logo = () => {
+    return (
+        <div className="logo-container">
+            <img src={logo} alt="dc-logo" />
+        </div>
+    );
+};
+
+export default Logo;
