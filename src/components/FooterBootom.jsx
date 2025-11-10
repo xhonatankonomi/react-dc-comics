4cd93e835a5a57780bf2d2098646f877e5c257a9
xhonatankonomi <xhoni8962@gmail.com>
2025-11-10 09:09:38 +0100

    Add FooterBottom and FooterLinks components for footer structure

---
+++ src/components/FooterBootom.jsx
@@ -0,0 +1,15 @@
+const FooterBottom = () => {
+    return (
+        <div className="footer-bottom-bar">
+            <button className="sign-up-button">SIGN-UP NOW!</button>
+            <div className="social-links">
+                <span>FOLLOW US</span>
+
+
+
+            </div>
+        </div>
+    );
+};
+
+export default FooterBottom;
---
+++ src/components/FooterLinks.jsx
@@ -0,0 +1,42 @@
+const FooterLinks = () => {
+    const footerData = [
+        {
+            title: "DC COMICS",
+            links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"]
+        },
+        {
+            title: "DC",
+            links: ["Terms Of Use", "Privacy Policy", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"]
+        },
+        {
+            title: "SITES",
+            links: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]
+        },
+        {
+            title: "SHOP",
+            links: ["Shop DC", "Shop DC Collectibles"]
+        },
+    ]
+
+    return (
+        <div className="footer-top">
+            <div className="link-section">
+                {footerData.map((col, index) => (
+                    <div className="link-columm" key={index}>
+                        <h3>{col.title}</h3>
+                        <ul>
+                            {col.links.map((link, linkIndex) => (
+                                <li key={linkIndex} about="#">{link}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+            <div className="dc-logo-bg">
+
+            </div>
+        </div>
+    );
+};
+
+export default FooterLinks;
